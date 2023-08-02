ac627d76cdc291e27fb179ea938b0c6408d8d752
seboo <25958061+seboo@users.noreply.github.com>
2023-08-02 15:57:23 +0200

    LUT-14949 : add external attribute source interface

---
+++ src/java/fr/paris/lutece/plugins/identitystore/v2/business/IExternalAttributeSource.java
@@ -0,0 +1,19 @@
+package fr.paris.lutece.plugins.identitystore.v2.business;
+
+import java.util.List;
+
+import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.search.CertifiedAttribute;
+
+public interface IExternalAttributeSource {
+	
+	/**
+	 * Complete the attribute list of the identity with external certified attributes
+	 * - Attributes must be certified with a known certification process
+	 * - do not add attributes that are already present in the central referential
+	 * - do not send those attributes to the central referential
+	 * 
+	 * @param cuid
+	 * @return the list of external attributes
+	 */
+	List<CertifiedAttribute> getAdditionnalAttributes( String cuid );
+}
