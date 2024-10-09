7bfd5fe5c6bff3b037c69cb0c7fa20020cd0af18
seboo <25958061+seboo@users.noreply.github.com>
2024-10-09 18:23:29 +0200

    LUT-28836 : add new task type

--- src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/dto/task/IdentityTaskType.java
+++ src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/dto/task/IdentityTaskType.java
@@ -3,5 +3,6 @@ package fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.task;
 public enum IdentityTaskType {
     EMAIL_VALIDATION_REQUEST,
     ACCOUNT_CREATION_REQUEST,
-    ACCOUNT_MERGE_REQUEST
+    ACCOUNT_MERGE_REQUEST,
+    ACCOUNT_IDENTITY_MERGE_REQUEST
 }
