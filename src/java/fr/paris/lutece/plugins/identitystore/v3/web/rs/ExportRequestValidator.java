a97ed5ccb6dd203221e1b1a30734a43c6aa4b9c6
Clément Roure <croure@yupiik.com>
2024-10-11 14:36:45 +0300

    refactor plugin-export

--- src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/ExportRequestValidator.java
+++ src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/ExportRequestValidator.java
@@ -1,7 +1,9 @@
 package fr.paris.lutece.plugins.identitystore.v3.web.rs;
 
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.exporting.ExportModelScheduleRequest;
+import fr.paris.lutece.plugins.identitystore.v3.web.rs.util.Constants;
 import fr.paris.lutece.plugins.identitystore.web.exception.IdentityStoreException;
+import fr.paris.lutece.plugins.identitystore.web.exception.RequestFormatException;
 import org.apache.commons.lang3.StringUtils;
 
 public class ExportRequestValidator extends RequestValidator {
@@ -17,9 +19,9 @@ public class ExportRequestValidator extends RequestValidator {
     private ExportRequestValidator() {
     }
 
-    public void validateScheduleExportRequest(final ExportModelScheduleRequest request) throws IdentityStoreException {
+    public void validateScheduleExportRequest(final ExportModelScheduleRequest request) throws RequestFormatException {
         if (request == null || request.getExportModelId() == null || StringUtils.isBlank(request.getEmail())) {
-            throw new IdentityStoreException("Schedule export request is null or empty");
+            throw new RequestFormatException("Schedule export request is null or empty", Constants.PROPERTY_REST_ERROR_SCHEDULE_EXPORT_REQUEST_NULL_OR_EMPTY);
         }
     }
 
